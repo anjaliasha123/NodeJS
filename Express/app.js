4ca95621b45edf40f4f4c7f76e7fde6fbee0e39b
anjaliasha123 <anjaliasha95@gmail.com>
2023-02-06 20:44:27 -0600

    express filtering routes

--- Express/app.js
+++ Express/app.js
@@ -1,28 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
 const app = express();
 app.use(bodyParser.urlencoded({extended: false})); //registering a parser
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
-
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>');
-});
-
-app.post('/product', (req, res, next)=>{
-    console.log('This is products page');
-    console.log(req.body.title);
-    res.redirect('/')
+//routing
+app.use('/admin',adminRouter.router); //filtering routes using /admin
+app.use(shopRouter.router);
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>404 Page not found!!</h1>');
 })
 
-app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
-});
+// app.use('/', (req, res, next) => {
+//     console.log('This always runs!');
+//     next();
+// });
+
+
 
 app.listen(3000);
